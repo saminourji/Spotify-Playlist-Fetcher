bc8825b8aa2c40863e2d7ed0298b557330b824ed
saminourji <saminourji23@gmail.com>
2023-07-10 18:12:49 +0200

    added gitignore file and started comparing frames

--- main.py
+++ main.py
@@ -1,19 +1,53 @@
 import cv2 as cv
 import numpy as np
+import math
 
-# video = "sample vids/slow_fullscreen_sample_vid.mp4"
+
+#INTIALIZE VARIABLES
 cap = cv.VideoCapture("sample vids/slow_fullscreen_sample_vid.mp4")
 count = 0
-success, frame = cap.read()
+success, frame = cap.read()00
+interval = 3
 
+# SAVE FRAMES AS IMAGES
 while success:
-    # count += 1
-
-    #save image every 5 frames: 
-    if count%5 == 0:
-        cv.imwrite("frame %s.jpg" % (count//5), frame)
-
+    count += 1
+    # prev_frame = frame
+    if count % interval == 0:
+        cv.imwrite("frames/frame %s.jpg" % (count//interval), frame)
     success, frame = cap.read()
-
-    # When everything done, release the capture
 cap.release()
+
+# COMPARE IMAGES TO REMOVE DUPLICATES
+def mse(img1, img2):
+    """
+    EFFECT: Computes the mean squared error of the two images
+    OUTPUT: mse, subtraction of first and second img
+    """
+    assert(img1.shape == img2.shape)
+    dim = img1.shape
+    diff = cv.subtract(img1, img2)
+    err = np.sum(diff**2)
+    return err/(float(dim[0]*dim[1])), diff
+
+def show_comparaison(img1, img2):
+    err, diff = mse(img1, img2)
+    print("Image matching error:", err)
+    cv.imshow("Difference", diff)
+    cv.waitKey(0)
+    cv.destroyAllWindows()
+
+lst = [1]
+print((count-1)//interval)
+#goes through every single create frame
+for i in range(1,math.ceil(count//interval)-1):
+    difference = mse(cv.imread("frames/frame %s.jpg" % str(i),0),cv.imread("frames/frame %s.jpg" % str(i+1),0))
+    if difference[0] > 2 and lst[-1] + 1 != i:
+        lst.append(i)
+print(lst)
+
+for n in lst:
+    img = cv.imread("frames/frame %s.jpg" % str(n), 0)
+    cv.imshow("frame", img)
+    cv.waitKey(0)
+    cv.destroyAllWindows()
