a46fb75d257ab5a89b94507c4af660933772b029
saminourji <saminourji23@gmail.com>
2023-07-18 23:25:08 +0200

    added fast and long sample vid

--- create_playlist.py
+++ create_playlist.py
@@ -11,7 +11,7 @@ print("Setting up Spotify API")
 scope = ["user-read-private", "playlist-modify-public"]
 auth_manager = SpotifyOAuth(client_id = apikeys.CLIENT_ID, client_secret = apikeys.CLIENT_SECRET, redirect_uri = "http://localhost:3000", scope = scope)
 sp = spotipy.Spotify(auth_manager=auth_manager)
-path = "sample vids/slow_fullscreen_sample_vid.mp4"
+path = "sample vids/fast_fullscreen_sample_vid.mp4"
 
 user_id = sp.me()["id"]
 
@@ -27,12 +27,12 @@ def search_best_url(tuple, n):
     # (title ratio, link, result nb)
     best = None
     
-    query = " - " + tuple[0] + " " + tuple[1].split(",")[0]
+    query = tuple[0] + " " + tuple[1].split(",")[0]
     search = sp.search(query, n, 0, "track")
-    # print(query)
+    print(" - " + query)
     for i in range(n):
         compare_title = similar(search["tracks"]["items"][i]["name"], tuple[0])
-        # print(f'TITLE score: {compare_title} between actual: {tuple[0]} and fetched: {search["tracks"]["items"][i]["name"]}')
+        print(f'TITLE score: {compare_title} between actual: {tuple[0]} and fetched: {search["tracks"]["items"][i]["name"]}')
         compare_artist = similar(search["tracks"]["items"][i]["artists"][0]["name"], tuple[1].split(",")[0])
         # print(f'ARTIST score: {compare_artist} between actual: {tuple[1].split(",")[0]} and fetched: {search["tracks"]["items"][i]["artists"][0]["name"]}')
         if best == None or (compare_title > best[0] and compare_artist > 0.2):
@@ -45,7 +45,7 @@ def search_best_url(tuple, n):
 
 new_playlist = sp.user_playlist_create(user_id, f"Playlist for {path}", True, False, "")
 
-song_info = get_music_titles(path, 3)
+song_info = get_music_titles(path, 1)
 # song_info = ['Beginnings (feat. East Forest)\nMC YOGI, East Forest\n', 'Yoste\n', 'The Healing\n\nEssie Jain\n', 'Gold Flow\n\nOJ Taz Rashid\n', 'Limitations\n\nEast Forest\n', 'Inspiration Drive\nDJ Taz Rashid\n', 'Chillaxing\n\nSol Rising\n', 'I Am - Krishan Liquid Mix\n\n. Nirinjan Kaur, Matthew Schoening, Ram D\n', 'Vega\n\nillo\n', 'Opening\n\nEssie Jain\n', 'Floating Sweetness\n\nDJ Drez, Domonic Dean Breaux\n', 'Kaonashi\n\nYoste\n', 'I Will Love You\nSol Rising\n', 'Waves\nIHF, Cyn\n', 'Wellspring - DJ Taz Rashid, Mo\n\nScott Nice, Nathan Hall, DJ Taz Rashid, Mor\n', 'Closing Meditation\n\nShantala\n', 'By Your Grace/Jai Gurudev\n\nKrishna Das\n', 'One River\nBenjy Wertheimer, John De Kadt\n', 'Shanti (Peace Out)\nMC YOGI\n', 'Angels Prayer\n\n»s Hoskins, Cat McCarthy, Manorama, Janak\n', 'In Dreams\nJai-Jagdeesh\n', 'Light Me Up\n\nDJ Drez, Marti Nikko\n', 'Heart Chakra\n\nBeautiful Chorus\n', 'Eyelids Gently Dreaming\n\nSteve Hauschildt\n', 'Angels Prayer\n\n-s Hoskins, Cat McCarthy, Manorama, Janak\n', 'Nectar Drop\nDJ Drez\n', 'Introspection\n\nLaraaji\n']
 
 omitted = 0
@@ -53,6 +53,7 @@ links = []
 print("Fetching song links")
 for info in song_info:
     split_info = [x for x in info.split("\n") if x != ""] #expected to put in format (title, artist) 
+    print(split_info)
     if len(split_info) == 2:
         ratio, link, _ = search_best_url(split_info, 3)
         if link not in links:
